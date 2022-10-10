09c53303608c759be5ca3c67c7eb9d8912b50988
Aayush <aayushpaliwal555@gmail.com>
2022-10-10 23:07:24 +0530

    Day3

---
+++ Break_and_Continue.js
@@ -0,0 +1,23 @@
+// Break 
+
+//It will stop execution of the loop when condition will becomes true 
+for(let i=0;i<=8;i++){
+    if(i==4){
+      break      
+    }
+    console.log(i)
+  }
+  console.log("Hello!!!")
+  console.log()
+  
+  
+  // Continue
+  
+  // It will skip execution of loop for i==4
+  for(let i=0;i<=8;i++){
+    if(i==4){
+      continue      
+    }
+    console.log(i)
+  }
+  console.log("Completed!!")
---
+++ Ternary_Operator.js
@@ -0,0 +1,16 @@
+let age=11
+let drink
+
+// Using if-else
+if(age>=10){
+  drink="Coffee"
+}
+else{
+  drink="Milk"
+}
+console.log(drink)
+
+
+// Using ternary operator
+drink=age>=10? "Coffee":"Milk"
+console.log(drink)
