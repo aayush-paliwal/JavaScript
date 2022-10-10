be510217e9f380f6295214ac52a4a24cdffee925
Aayush <aayushpaliwal555@gmail.com>
2022-10-10 23:07:24 +0530

    Day3

---
+++ Strings.js
@@ -0,0 +1,20 @@
+let a = "Strings"
+
+//             S t r i n g s
+// Index:      0 1 3 2 3 4 5 
+
+console.log("Printing element at index 3:" + a[3])
+
+// Space is also counted as a length of string
+console.log("Length of string is: " + a.length)
+
+// String concatenation
+let b="aayush"
+let c="45"
+let about="my name is "+b+" and I am "+c+" years old"
+console.log(about)
+
+              // OR
+
+let about1=`my name is ${b} and I am ${c} years old`
+console.log(about1)
